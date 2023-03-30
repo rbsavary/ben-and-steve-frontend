41d71fbd0a3cc403d322cb0d7ac76e43ae17f3ae
Bob <rbsavary88@gmail.com>
2023-03-30 14:09:01 -0400

    resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,70 +1,67 @@
-import { useState, useEffect  } from 'react'
-import axios from 'axios'
-import Post from './components/Post'
-import Add from './components/Add'
-import Edit from './components/Edit'
-import Header from './components/Header'
-import About from './components/About'
+import './App.css';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
+import Posts from './components/Posts';
+import Add from './components/Add';
+import Contact from "./components/Contact";
+// import Edit from './components/Edit';
+import Header from './components/Header';
+import About from './components/About';
 
 const App = () => {
-  // State Variables
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   const getPosts = () => {
-    axios.get('http://127.0.0.1:3000/posts')
-    .then((response) => setPosts(response.data), (err) => console.log(err))
-    .catch((error) => console.log(error))
-  }
+    axios.get('http://127.0.0.1:3000/')
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.log(error))
+  };
 
   const handleCreate = (data) => {
-    console.log(data)
     axios.post('http://127.0.0.1:3000/posts', data)
-    .then((response) => {
-      console.log(response)
-      let newPosts = [...posts, response.data]
-      setPosts(newPosts)
-    })
-  }
-
-  const handleDelete = (deletedPost) => {
-    axios.delete('http://127.0.0.1:3000/posts/' + deletedPost._id)
-    .then(() => {
-      let newPosts = posts.filter((post) => {
-        return post._id !== deletedPost._id
+      .then((response) => {
+        let newPosts = [...posts, response.data]
+        setPosts(newPosts);
       })
-      setPosts(newPosts)
-    })
-  }
+  };
 
-  const handleEdit = (data) => {
-    axios.put('http://127.0.0.1:3000/posts/' + data._id, data)
-    .then(() => {
-      let newPosts = posts.map((post) => {
-        return post._id !== data._id ? post : data
-      })
-      setPosts(newPosts)
-    })
-  }
+  // const handleDelete = (deletedPost) => {
+  //   axios.delete('http://127.0.0.1:3000/posts/' + deletedPost._id)
+  //     .then((response) => {
+  //       let newPosts = posts.filter((post) => {
+  //         return post._id !== deletedPost._id
+  //       })
+  //       setPosts(newPosts);
+  //     })
+  // };
+
+  // const handleEdit = (data) => {
+  //   axios.put('http://127.0.0.1:3000/posts/' + data._id, data)
+  //     .then((response) => {
+  //       let newPosts = posts.map((post) => {
+  //         return post._id !== data._id ? post : data
+  //       })
+  //       setPosts(newPosts);
+  //     })
+  // };
 
   useEffect(() => {
     getPosts()
-  }, [])
+  }, []);
 
   return (
-  <>
-    <Header/>
-    <About/>
-    <Add handleCreate={handleCreate}/>
-    {posts.map((post) => (
-      <>
-        <Post post={post}/>
-        <Edit post={post} handleEdit={handleEdit} />
-        <button onClick={ () => {handleDelete(post) } }>Delete</button>
-      </>
-      )
-    )
-    }
-  </>
+    <BrowserRouter>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Posts posts={posts} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/add" element={<Add handleCreate={handleCreate} />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
